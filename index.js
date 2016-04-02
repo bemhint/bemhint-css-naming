5d7db565a9d75ed7ce26745a4fc9b0f814c396ca
Dmitry Andriyanov <dima117a@yandex-team.ru>
2016-04-02 14:03:13 +0300

    add selector parser

--- index.js
+++ index.js
@@ -1,4 +1,27 @@
-var postcss = require('postcss');
+var postcss = require('postcss'),
+    parser = require('postcss-selector-parser'),
+    transform = function (selectors) {
+
+        //console.log(JSON.stringify(selectors, function( key, value) {
+        //  if( key == 'parent') { return 'xxx';}
+        //  else {return value;}
+        //}));
+
+
+        selectors.eachClass(function (cssClass) {
+
+            //entity.addError({
+            //                   msg: 'test',
+            //                   tech: tech.name,
+            //                   value: 'XXX'
+            //               });
+
+            // do something with the selector
+            console.log(cssClass);
+        });
+
+        //console.log(Object.keys(selectors));
+    };
 
 module.exports = {
     forEntityTech: function(tech, techConfig, entity) {
@@ -6,18 +29,8 @@ module.exports = {
         var data = postcss.parse(tech.content);
 
         data.nodes.forEach(function(rule) {
-            if (rule.type !== 'rule') {
-                return;
-            }
-
-            console.log(rule.selector);
+            rule.type === 'rule' && parser(transform).process(rule.selector);
         });
-
-        entity.addError({
-                    msg: 'test',
-                    tech: tech.name,
-                    value: 'XXX'
-                });
     }
 };
 
