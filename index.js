16b525e9d192930919827813d4d7e5ddd8bb0a72
Evgeniy Konstantinov <sipayrt@yandex-team.ru>
2016-04-02 17:46:36 +0300

    Add more info for errors

--- index.js
+++ index.js
@@ -1,36 +1,46 @@
 var bemNaming = require('bem-naming'),
     postcss = require('postcss'),
-    parser = require('postcss-selector-parser');
+    parser = require('postcss-selector-parser'),
+    util = require('util');
 
 module.exports = {
     forEachTech: function(tech, entity, config) {
         var data = postcss.parse(tech.content);
 
+        function addError(msg, selector, line, column) {
+            entity.addError({
+                msg: msg,
+                tech: tech.name,
+                value: util.format('%s at line %s, column %s', selector, line, column)
+            });
+        }
+
         data.nodes.forEach(function(rule) {
             rule.type === 'rule' && parser(function(selectors) {
 
                 selectors.each(function(selector) {
-                    var hasTargetBlock = false;
+                    var hasTargetBlock = false,
+                        ruleStart = rule.source.start;
 
                     selector.eachClass(function (cssClass) {
-                        var cssEntity = bemNaming.parse(cssClass.value);
+                        var cssEntity = bemNaming.parse(cssClass.value),
+                            cssClassStart = cssClass.source.start,
+                            line = ruleStart.line + cssClassStart.line - 1;
 
                         if (cssEntity) {
                             hasTargetBlock = hasTargetBlock || (cssEntity.block === tech.entity.block);
                         } else {
-                            entity.addError({
-                                msg: 'invalid class name in ' + rule.selector,
-                                tech: tech.name,
-                                value: cssClass.value
-                            });
+                            addError('invalid class naming', rule.selector, line, cssClassStart.column);
                         }
                     });
 
-                    !hasTargetBlock && entity.addError({
-                        msg: 'invalid block in ' + rule.selector,
-                        tech: tech.name,
-                        value: selector.toString()
-                    });
+                    hasTargetBlock || addError(
+                        'selector does not contain block name specified in the file name',
+                        rule.selector,
+                        ruleStart.line,
+                        ruleStart.column
+                    );
+
                 });
             }).process(rule.selector);
         });
