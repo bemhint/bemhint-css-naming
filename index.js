9f5b555cefa764f3876b7a06cb6f27447e1d9cb3
Dmitry Andriyanov <dima117a@yandex-team.ru>
2016-04-02 16:07:59 +0300

    validate target block name

--- index.js
+++ index.js
@@ -4,23 +4,34 @@ var bemNaming = require('bem-naming'),
 
 module.exports = {
     forEntityTech: function(tech, techConfig, entity) {
-
-        //console.log(tech, entity);
-
         var data = postcss.parse(tech.content);
 
         data.nodes.forEach(function(rule) {
             rule.type === 'rule' && parser(function(selectors) {
 
-                selectors.eachClass(function (cssClass) {
+                selectors.each(function(selector) {
+                    var hasTargetBlock = false;
 
-                    !bemNaming.validate(cssClass.value) && entity.addError({
-                        msg: 'invalid class name',
+                    selector.eachClass(function (cssClass) {
+                        var cssEntity = bemNaming.parse(cssClass.value);
+
+                        if (cssEntity) {
+                            hasTargetBlock = hasTargetBlock || (cssEntity.block === tech.entity.block);
+                        } else {
+                            entity.addError({
+                                msg: 'invalid class name in ' + rule.selector,
+                                tech: tech.name,
+                                value: cssClass.value
+                            });
+                        }
+                    });
+
+                    !hasTargetBlock && entity.addError({
+                        msg: 'invalid block in ' + rule.selector,
                         tech: tech.name,
-                        value: cssClass.value
+                        value: selector.toString()
                     });
                 });
-
             }).process(rule.selector);
         });
     }
