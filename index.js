f86ecc0abf9ad9bb63398421e338b786ece8bc20
Dmitry Andriyanov <dima117a@yandex-team.ru>
2016-04-02 14:12:02 +0300

    validate class names

--- index.js
+++ index.js
@@ -1,35 +1,27 @@
-var postcss = require('postcss'),
-    parser = require('postcss-selector-parser'),
-    transform = function (selectors) {
-
-        //console.log(JSON.stringify(selectors, function( key, value) {
-        //  if( key == 'parent') { return 'xxx';}
-        //  else {return value;}
-        //}));
-
-
-        selectors.eachClass(function (cssClass) {
-
-            //entity.addError({
-            //                   msg: 'test',
-            //                   tech: tech.name,
-            //                   value: 'XXX'
-            //               });
-
-            // do something with the selector
-            console.log(cssClass);
-        });
-
-        //console.log(Object.keys(selectors));
-    };
+var bemNaming = require('bem-naming'),
+    postcss = require('postcss'),
+    parser = require('postcss-selector-parser');
 
 module.exports = {
     forEntityTech: function(tech, techConfig, entity) {
 
+        //console.log(tech, entity);
+
         var data = postcss.parse(tech.content);
 
         data.nodes.forEach(function(rule) {
-            rule.type === 'rule' && parser(transform).process(rule.selector);
+            rule.type === 'rule' && parser(function(selectors) {
+
+                selectors.eachClass(function (cssClass) {
+
+                    !bemNaming.validate(cssClass.value) && entity.addError({
+                        msg: 'invalid class name',
+                        tech: tech.name,
+                        value: cssClass.value
+                    });
+                });
+
+            }).process(rule.selector);
         });
     }
 };
