f1307b1b6ca18fe4326209d44f43cf9a7697148c
Evgeniy Konstantinov <sipayrt@yandex-team.ru>
2016-04-03 15:52:42 +0300

    Add more tests

--- test/css-naming.js
+++ test/css-naming.js
@@ -4,12 +4,74 @@ var CssNaming = require('../lib/css-naming'),
     postcss = require('postcss');
 
 describe('css naming', function() {
-    it('should add error for invalid class name', function() {
-        var errorCallback = sinon.spy(),
-            validator = new CssNaming(null, errorCallback);
+    describe('should fail', function() {
+        it('if got error while css parse', function() {
+            var errorCallback = sinon.spy(),
+                validator = new CssNaming(null, errorCallback);
 
-        validator.validateSelectors('.block__elem__elem{}', 'block');
+            validator.validateSelectors('.block__elem__elem{', 'block');
 
-        assert.called(errorCallback);
+            assert.called(errorCallback);
+            assert.calledWith(errorCallback, sinon.match(/Failed to check/));
+        });
+
+        it('for invalid class name', function() {
+            var errorCallback = sinon.spy(),
+                validator = new CssNaming(null, errorCallback);
+
+            validator.validateSelectors('.block__elem__elem{}', 'block');
+
+            assert.called(errorCallback);
+            assert.calledWith(errorCallback, sinon.match(/Invalid class naming/));
+        });
+
+        it('if selector does not contain block name', function() {
+            var errorCallback = sinon.spy(),
+                validator = new CssNaming(null, errorCallback);
+
+            validator.validateSelectors('.another-block{}', 'block');
+
+            assert.called(errorCallback);
+            assert.calledWith(errorCallback, sinon.match(/not contain block name/));
+        });
+
+        it('if selector was added to exclude but does not contain block name', function() {
+            var errorCallback = sinon.spy(),
+                validator = new CssNaming(['e_x_c_l_u_d_e'], errorCallback);
+
+            validator.validateSelectors('.e_x_c_l_u_d_e{}', 'block');
+
+            assert.called(errorCallback);
+            assert.calledWith(errorCallback, sinon.match(/not contain block name/));
+        });
+    });
+
+    describe('should pass', function() {
+        it('if selector contains block name', function() {
+            var errorCallback = sinon.spy(),
+                validator = new CssNaming(null, errorCallback);
+
+            validator.validateSelectors('.another-block .block{}', 'block');
+
+            assert.notCalled(errorCallback);
+        });
+
+        it('if selector is related with current block', function() {
+            var errorCallback = sinon.spy(),
+                validator = new CssNaming(null, errorCallback);
+
+            validator.validateSelectors('.block__elem_mod{}', 'block');
+
+            assert.notCalled(errorCallback);
+        });
+
+        it('if wrong class was added to excludes', function() {
+            var errorCallback = sinon.spy(),
+                validator = new CssNaming(['test-*'], errorCallback);
+
+            validator.validateSelectors('.block .test-e_x_c_l_u_d_e_d{}', 'block');
+
+            assert.notCalled(errorCallback);
+        });
     });
 });
